1a5341b2eff037f17b2082e429a7101022758297
brianrho <brianrho94@gmail.com>
2019-01-03 08:46:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+﻿## Server/PC-side matching (in C) for R3xx/FPMxx/ZFMxx fingerprint sensors
+
+This is based on a poorly documented DLL I found in the SDK which performs the actual matching.
+Tested only on Windows but not thoroughly, though results are promising and almost
+the same (in terms of fingerprint match scores) as those from the sensor itself. 
+
+The DLL matching functions seem to accept only 256-byte arrays, which is consistent
+with the typical 512-byte template that looks like it's simply two 256-byte templates concatenated.
+This means that for matching, only first 256 bytes of a template are actually needed.
+
+Use at your own risk and please let me know if you discover anything.
---
+++ src/fmatch.c
@@ -0,0 +1,64 @@
+/*
+ ============================================================================
+ Name        : fmatch.c
+ Author      : Brian Ejike (2018, brianrho94@gmail.com)
+ Version     :
+ Copyright   : MIT licence
+ Description : Template match
+ ============================================================================
+ */
+
+/* Make sure to link the necessary ARTH_DLL.dll
+ * On windows, use this with gcc linker options:
+ * 		-L"C:\library-path" -Xlinker --enable-stdcall-fixup -m32
+ *
+ * The prototypes are in matching.h
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include "matching.h"
+#include "templates.h"
+
+#define MATCH_THRESHOLD		50
+
+void match_pair(unsigned char * one, unsigned char * two);
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int score;
+
+	/* perform a bunch of matches and check how long they take */
+	clock_t begin, end;
+	begin = clock();
+	for (int i = 0; i < 1000; i++) {
+		score = Match2Fp(template18, template16);
+	}
+	end = clock();
+
+	printf("Match result: %d, took %lf secs.\n", score, (double)(end - begin) / CLOCKS_PER_SEC);
+	if (score > MATCH_THRESHOLD)
+		printf("Prints match.\n");
+	else
+		printf("Prints dont match!\n");
+
+	return 0;
+}
+
+/* print scores from all possible combos */
+void match_pair(unsigned char * one, unsigned char * two) {
+	int score = Match2Fp(one, two);
+	printf("Score 1: %d\r\n", score);
+
+	score = Match2Fp(one, two+256);
+	printf("Score 2: %d\r\n", score);
+
+	score = Match2Fp(one+256, two);
+	printf("Score 3: %d\r\n", score);
+
+	score = Match2Fp(one+256, two+256);
+	printf("Score 4: %d\r\n", score);
+}
